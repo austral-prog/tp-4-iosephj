49f8fc06f2bcb68f28bbe83d02de51e447b2fc94
iosephj <juarez.jose.g@gmail.com>
2025-04-11 11:57:20 -0300

    Add files via upload

--- line.py
+++ line.py
@@ -1,2 +1,18 @@
 def line():
-    print("TO DO")
+    #print("TO DO")
+    A = float(input("Ingrese el coeficiente A: "))
+    B = float(input("Ingrese el coeficiente B: "))
+    X1 = float(input("Ingrese el coeficiente X1: "))
+    X2 = float(input("Ingrese el coeficiente X2: "))
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
+    print("\nPara la siguiente ecuación:")
+    print(f"\tY = {A}X + {B}")
+    Y1 = A*X1 + B
+
+#line()
+
+
+
